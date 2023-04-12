e3faed74902d5bbc3b6678be43f85ec7e8f18a71
Fithamlak <justiceofgod01@gmail.com>
2023-04-12 16:26:55 +0300

    add all schemas in index file

---
+++ server/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import abouts from './abouts'
+import contact from './contact'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  testimonials,
+  abouts,
+  contact,
+  brands,
+  experiences,
+  skills,
+  workExperience,
+  works,
+]
